28d9ee498c4def74f51a84ddd5eaf8913869de62
Iksan Maricar <iksansahib@gmail.com>
2018-09-21 16:05:08 +0700

    bug fix token undefined

--- server/witClient.js
+++ server/witClient.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const { witToken } = require('../global');
+console.log(witToken);
 module.exports.ask = (message) => {
     let res = null
     res = axios.get(`https://api.wit.ai/message?v=20180920&q=${message}`,{
