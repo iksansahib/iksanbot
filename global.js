73372104cdefaf567aef0d6cf6d5229b4ebd6568
Iksan Maricar <iksansahib@gmail.com>
2018-09-21 16:12:35 +0700

    add token

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
 module.exports =  {
-    token : 'xxx',
-    witToken : 'xxx'
+    token : 'xoxb-438025246929-438804019074-izhU2C4XmGmOVedcq3IDvFor',
+    witToken : 'XHGHR35RLHKUZZFEYRMMWQ6KRAJDWWMG'
 };
